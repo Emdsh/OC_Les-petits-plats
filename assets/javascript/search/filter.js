aea8d27af6d0ce6b5a0b038388fbdcaf57efdd26
Emdsh <embitiousdev@gmail.com>
2022-01-31 17:18:48 +0100

    fix(search): switch to intersection search instead of reunion search

--- assets/javascript/search/filter.js
+++ assets/javascript/search/filter.js
@@ -1,35 +1,49 @@
 /* eslint-disable consistent-return */
 
 import filterFilters from './filterFilters.js';
+import search from './search.js';
 
 // filter recipes depending on the selected filters
 function filter() {
   // get every selected filters
-  const filters = Array.from(document.querySelectorAll('.filters__item'));
+  const filters = Array.from(document.querySelectorAll('.filters__item')).map((list) => list.innerText.toLowerCase());
   const recipes = document.querySelectorAll('.recipe-tile');
 
-  if (filters.length < 1) {
-    for (let h = 0; h < recipes.length; h += 1) {
-      recipes[h].style.display = 'block';
+  recipes.forEach((recipe) => {
+    if (filters.length < 1) {
+      recipe.style.display = 'block';
+      return;
     }
-    return filterFilters();
-  }
 
-  // hide all recipes by default
-  for (let h = 0; h < recipes.length; h += 1) {
-    recipes[h].style.display = 'none';
-  }
+    // hide every recipe
+    recipe.style.display = 'none';
 
-  // display recipes that contain the filter
-  for (let j = 0; j < recipes.length; j += 1) {
-    for (let k = 0; k < filters.length; k += 1) {
-      if (recipes[j].innerText.toLowerCase().includes(filters[k].innerText.toLowerCase())) {
-        recipes[j].style.display = 'block';
+    // create a checksum array
+    let includesAllFilters = Array(filters.length).fill(false);
+
+    // display recipes that match with the filters
+    filters.forEach((filterText) => {
+      if (recipe.innerText.toLowerCase().includes(filterText)) {
+        includesAllFilters.shift();
+        includesAllFilters.push(true);
       }
-    }
-  }
+    });
 
+    includesAllFilters = includesAllFilters.reduce((a, b) => a && b);
+
+    if (includesAllFilters === true) {
+      recipe.style.display = 'block';
+    }
+  });
+
+  // filter filters depending on the main search bar input
   filterFilters();
+
+  // check if search bar has a value
+  const searchBarValue = document.getElementById('search-bar').value;
+  if (searchBarValue.length > 2) {
+    search(true);
+  }
 }
 
 export default filter;
--- assets/javascript/search/search.js
+++ assets/javascript/search/search.js
@@ -4,37 +4,39 @@ import error from './error.js';
 import filterFilters from './filterFilters.js';
 
 // filter recipes depending on the main search bar input
-function search() {
-  const searchBar = document.getElementById('search-bar');
+function search(isComingFromFilter) {
+  const searchBarValue = document.getElementById('search-bar').value;
+  let recipes = document.querySelectorAll('.recipe-tile');
 
-  searchBar.addEventListener('input', () => {
-    const recipes = document.querySelectorAll('.recipe-tile');
+  if (isComingFromFilter === true) {
+    recipes = document.querySelectorAll('.recipe-tile:not([style*="display: none;"])');
+  }
 
-    // if input is under 3 letters, display all recipe tiles
-    if (searchBar.value.length < 3) {
-      for (let g = 0; g < recipes.length; g += 1) {
-        recipes[g].style.display = 'block';
-      }
-      return filterFilters();
+  // if input is under 3 letters, display all recipe tiles
+  if (searchBarValue.length < 3) {
+    for (let g = 0; g < recipes.length; g += 1) {
+      recipes[g].style.display = 'block';
     }
-    // start filtering once input is at least 3 letters
-    // hide all recipes by default
-    for (let h = 0; h < recipes.length; h += 1) {
-      recipes[h].style.display = 'none';
+    return filterFilters();
+  }
+  // start filtering once input is at least 3 letters
+  // hide all recipes by default
+  for (let h = 0; h < recipes.length; h += 1) {
+    recipes[h].style.display = 'none';
+  }
+
+  // display recipes that contain the input in the search bar
+  for (let m = 0; m < recipes.length; m += 1) {
+    if (recipes[m].innerText.toLowerCase().includes(searchBarValue.toLowerCase())) {
+      recipes[m].style.display = 'block';
     }
+  }
 
-    // display recipes that contain the input in the search bar
-    for (let m = 0; m < recipes.length; m += 1) {
-      if (recipes[m].innerText.toLowerCase().includes(searchBar.value.toLowerCase())) {
-        recipes[m].style.display = 'block';
-      }
-    }
+  // will display an error message if the page is empty
+  error();
 
-    // filter filters depending on the main search bar input
-    filterFilters();
-
-    error();
-  });
+  // filter filters depending on the main search bar input
+  filterFilters();
 }
 
 export default search;
