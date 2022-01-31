5536a4b48f4c038a6369b17d6fbb13543ba82549
Emdsh <embitiousdev@gmail.com>
2022-01-31 17:51:50 +0100

    fix(rollback): revert to native loops

--- assets/javascript/search/filter.js
+++ assets/javascript/search/filter.js
@@ -6,35 +6,45 @@ import search from './search.js';
 // filter recipes depending on the selected filters
 function filter() {
   // get every selected filters
-  const filters = Array.from(document.querySelectorAll('.filters__item')).map((list) => list.innerText.toLowerCase());
+  const filters = Array.from(document.querySelectorAll('.filters__item'));
   const recipes = document.querySelectorAll('.recipe-tile');
 
-  recipes.forEach((recipe) => {
-    if (filters.length < 1) {
-      recipe.style.display = 'block';
-      return;
+  if (filters.length < 1) {
+    for (let h = 0; h < recipes.length; h += 1) {
+      recipes[h].style.display = 'block';
     }
+    filterFilters();
+    // check if search bar has a value
+    const searchBarValue = document.getElementById('search-bar').value;
+    if (searchBarValue.length > 2) {
+      search(true);
+    }
+    return;
+  }
 
-    // hide every recipe
-    recipe.style.display = 'none';
+  // hide all recipes by default
+  for (let h = 0; h < recipes.length; h += 1) {
+    recipes[h].style.display = 'none';
+  }
 
+  // display recipes that contain every filters
+  for (let j = 0; j < recipes.length; j += 1) {
     // create a checksum array
     let includesAllFilters = Array(filters.length).fill(false);
 
-    // display recipes that match with the filters
-    filters.forEach((filterText) => {
-      if (recipe.innerText.toLowerCase().includes(filterText)) {
+    for (let k = 0; k < filters.length; k += 1) {
+      if (recipes[j].innerText.toLowerCase().includes(filters[k].innerText.toLowerCase())) {
         includesAllFilters.shift();
         includesAllFilters.push(true);
       }
-    });
+    }
 
     includesAllFilters = includesAllFilters.reduce((a, b) => a && b);
 
     if (includesAllFilters === true) {
-      recipe.style.display = 'block';
+      recipes[j].style.display = 'block';
     }
-  });
+  }
 
   // filter filters depending on the main search bar input
   filterFilters();
